4e33d607fcc9031c833d98a8bec067e3f52c3922
Mothkuri Prince <130035014+princemothkuri@users.noreply.github.com>
2024-03-10 23:14:06 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,14 +1,6 @@
 const mongoose = require("mongoose");
 const db = process.env.DATABASE;
 
-// Set global Mongoose options (if needed)
-
-mongoose.set('bufferCommands', false); // Disable command buffering
-mongoose.set('useUnifiedTopology', true); // Enable useUnifiedTopology
-mongoose.set('socketTimeoutMS', 30000); // Set socket timeout (in milliseconds)
-mongoose.set('connectTimeoutMS', 30000); // Set connection timeout (in milliseconds)
-mongoose.set('serverSelectionTimeoutMS', 30000); // Set server selection timeout (in milliseconds)
-
 mongoose
  .connect(db, {
   useNewUrlParser: true,
